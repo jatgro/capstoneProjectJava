c35169e45e7715e5ab34ae2b5797da7c02ffdc48
JG_1 <er.jatin2112@gmail.com>
2020-02-29 13:27:02 +0530

    resolved a bug

--- capstoneProject/src/main/java/com/capstone/project/repository/UserReposirtory.java
+++ capstoneProject/src/main/java/com/capstone/project/repository/UserReposirtory.java
@@ -3,11 +3,13 @@ package com.capstone.project.repository;
 import java.util.Optional;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import com.capstone.project.entity.User;
 
 // Implementation of method is provided by JPA automatically
 
+@Repository
 public interface UserReposirtory extends JpaRepository<User, Long> {
 	Optional<User> findByUserName(String username);
 }
